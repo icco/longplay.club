a28da443b09546ab6fe25b228b0d7dba58f4d8ce
Nat Welch <nat@natwelch.com>
2016-05-22 02:32:36 +0000

    Copying things from https://github.com/jxnblk/Microbeats

---
+++ entry.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var Root = require('./fe/Root.jsx');
+
+if (typeof document !== 'undefined') {
+  var initialProps = JSON.parse(document.getElementById('initial-props').innerHTML);
+  React.render(React.createElement(Root, initialProps), document);
+}
+
+// Exported static site renderer:
+module.exports = function render(locals, callback) {
+  var html = React.renderToString(React.createElement(Root, locals));
+  callback(null, '<!DOCTYPE html>' + html);
+};
