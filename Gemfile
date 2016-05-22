6616b5868e6be486d3ab173316e9a01ceb28c616
Nat Welch <nat@natwelch.com>
2016-05-22 01:37:47 +0000

    progress

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source "https://rubygems.org"
+
+ruby "2.3.1"
+
+gem "erubis", "~> 2.7"
+gem "oj"
+gem "rake"
+gem "sinatra"
+gem "thin"
+
+# For dev
+group :development do
+  gem "bundler"
+  gem "heroku"
+  gem "shotgun"
+end
---
+++ site.rb
@@ -0,0 +1,10 @@
+RACK_ENV ||= ENV["RACK_ENV"] ||= "development" unless defined?(RACK_ENV)
+
+require "rubygems" unless defined?(Gem)
+require "bundler/setup"
+Bundler.require(:default, RACK_ENV)
+
+get '/' do
+  @sounds = Dir.glob('public/sounds/*.mp3').sample(10)
+  erb :index
+end
