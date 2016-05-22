4b4ee11fa0b2df83d6ef3126e595308fe5818075
Nat Welch <nat@natwelch.com>
2016-05-22 02:11:25 +0000

    pogress

--- site.rb
+++ site.rb
@@ -5,6 +5,6 @@ require "bundler/setup"
 Bundler.require(:default, RACK_ENV)
 
 get '/' do
-  @sounds = Dir.glob('public/sounds/*.mp3').sample(10)
+  @sounds = Dir.glob('public/sounds/*.mp3').sample(10).map {|s| File.basename(s) }
   erb :index
 end
